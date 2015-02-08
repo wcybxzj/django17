6530dc8f0f59a31c7302316a5a49a6bf9c2a8bff
wcybxzj <virgo86ybx@gmail.com>
2015-02-08 20:24:27 +0800

    finish

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
@@ -6,5 +7,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    #namespace 命名空间影响模板
+    # url 'polls:detail' 和 views.py中reverse('polls:results',....
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -13,7 +13,9 @@ class Question(models.Model):
         return u"%s" % self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now >= self.pub_date >= now - datetime.timedelta(days=1)
+
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
@@ -22,5 +24,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
     def __str__(self):              # __unicode__ on Python 2
         return self.choice_text
